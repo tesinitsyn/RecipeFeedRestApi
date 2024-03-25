84e8b7e64b1ac27e3ebde505ef4b951ddaf25dcc
tesinitsyn <sitiev174@gmail.com>
2024-03-25 12:49:14 +0300

    finished MyUserDetailsService

--- src/main/java/org/tesinitsyn/recipefeedrestapi/user/service/MyUserDetailsService.java
+++ src/main/java/org/tesinitsyn/recipefeedrestapi/user/service/MyUserDetailsService.java
@@ -3,19 +3,20 @@ package org.tesinitsyn.recipefeedrestapi.user.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.tesinitsyn.recipefeedrestapi.user.config.MyUserDetails;
 import org.tesinitsyn.recipefeedrestapi.user.model.User;
 import org.tesinitsyn.recipefeedrestapi.user.respository.UserRepository;
 
 import java.util.Optional;
 
 public class MyUserDetailsService implements UserDetailsService {
-
     private UserRepository repository;
 
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = repository.findByName(username);
-        return user.map
+        return user.map(MyUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(username + "not found"));
     }
 }
