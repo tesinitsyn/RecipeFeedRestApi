a8705b5f27903f42853270e14998eb1fb5e2d047
tesinitsyn <sitiev174@gmail.com>
2024-03-25 13:01:06 +0300

    added SecurityConfig

--- src/main/java/org/tesinitsyn/recipefeedrestapi/user/model/User.java
+++ src/main/java/org/tesinitsyn/recipefeedrestapi/user/model/User.java
@@ -2,11 +2,16 @@ package org.tesinitsyn.recipefeedrestapi.user.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 
 public class User {
