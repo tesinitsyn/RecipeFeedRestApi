0bd15bda2bf395f8c11ac24fd41f8b6074f1af58
tesinitsyn <sitiev174@gmail.com>
2024-03-26 16:13:24 +0300

    added CustomUserDetailsService

--- src/main/java/org/tesinitsyn/recipefeedrestapi/repository/RoleRepository.java
+++ src/main/java/org/tesinitsyn/recipefeedrestapi/repository/RoleRepository.java
@@ -1,10 +1,12 @@
 package org.tesinitsyn.recipefeedrestapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tesinitsyn.recipefeedrestapi.model.Role;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     Optional<Role> findByName(String name);
 }
--- src/main/java/org/tesinitsyn/recipefeedrestapi/repository/UserRepository.java
+++ src/main/java/org/tesinitsyn/recipefeedrestapi/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package org.tesinitsyn.recipefeedrestapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tesinitsyn.recipefeedrestapi.model.UserEntity;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByUsername(String username);
     Boolean existsByUsername(String username);
