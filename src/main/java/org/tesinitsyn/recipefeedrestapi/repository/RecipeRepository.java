81edb1850aca509e5dfddb38fc4d359c1c384ec8
tesinitsyn <sitiev174@gmail.com>
2024-04-03 09:20:59 +0300

    added findRecipesByRecipeName in RecipeRepository

--- src/main/java/org/tesinitsyn/recipefeedrestapi/repository/RecipeRepository.java
+++ src/main/java/org/tesinitsyn/recipefeedrestapi/repository/RecipeRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.tesinitsyn.recipefeedrestapi.model.Recipe;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Integer>{
+    Optional<List<Recipe>> findRecipesByRecipeName(String name);
+
 
 }
