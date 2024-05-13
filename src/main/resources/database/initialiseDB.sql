a7dc09a2f2c7a28bf2612656cc984d197535d41c
tesinitsyn <sitiev174@gmail.com>
2024-05-13 12:58:23 +0300

    added auth controller, new sql init table

--- src/main/resources/database/initialiseDB.sql
+++ src/main/resources/database/initialiseDB.sql
@@ -1,12 +1,19 @@
 CREATE TABLE IF NOT EXISTS recipes
 (
-    id    BIGSERIAL PRIMARY KEY,
-    recipe_name VARCHAR(254) NOT NULL,
+    id           BIGSERIAL PRIMARY KEY,
+    recipe_name  VARCHAR(254) NOT NULL,
     ingredients  VARCHAR(200) NOT NULL,
-    description VARCHAR(254) NOT NULL,
+    description  VARCHAR(254) NOT NULL,
     time_to_cook VARCHAR(20)  NOT NULL,
     recipe_likes BIGSERIAL,
-    image_data text
+    image_data   text
 );
 
 
+CREATE TABLE users
+(
+    id       SERIAL PRIMARY KEY,
+    email    VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255)        NOT NULL,
+    role     VARCHAR(255)        NOT NULL
+);
